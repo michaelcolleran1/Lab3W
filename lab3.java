713dcae058f1a4280094770b3f260ebe8c294884
michaelcolleran1 <144271462+michaelcolleran1@users.noreply.github.com>
2023-09-27 14:31:05 -0400

    Add files via upload

--- lab3.java
+++ lab3.java
@@ -1,9 +1,14 @@
 
+
+/*Final file to run methods from team
+ * Michael Colleran
+ */
+
 public class lab3 extends myXMLValidator {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println();
 		
 	}
 }
